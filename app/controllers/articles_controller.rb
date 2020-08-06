278c64e68e7a1b9c4aa71c6788b1cd31ff0e4186
saqla <whitecross8fortune8fatedress@gmail.com>
2020-08-06 21:42:47 +0900

    rubocop の導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,11 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update]
+  before_action :set_article, only: %i[show edit update]
 
   def index
     @articles = Article.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @article = Article.new
@@ -22,8 +21,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @article.update(article_params)
@@ -40,8 +38,8 @@ class ArticlesController < ApplicationController
     redirect_to root_path, notice: '削除に成功しました'
   end
 
-
   private
+
   def article_params
     params.require(:article).permit(:title, :content)
   end
@@ -49,6 +47,4 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id])
   end
-
-
 end
