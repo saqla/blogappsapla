75538e328ccbd0af82b3748bf68b1b7d0eef1cfb
saqla <whitecross8fortune8fatedress@gmail.com>
2020-08-10 23:33:23 +0900

    自分が書いた記事かの判定 わかりにくいコードはmodelに書いてメソッド化してから用いる

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+
+  def has_weitten?(article)
+    articles.exists?(id: article.id)
+  end
+
 end
