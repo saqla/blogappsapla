8df27a6bbbd5eb9b11bb4c8e8876b89473b460b8
saqla <whitecross8fortune8fatedress@gmail.com>
2020-09-16 17:35:10 +0900

    アンフォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,17 +49,14 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
-
+    user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = following_relationships.find_by!(following_id: user.id)
+
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -78,4 +75,14 @@ class User < ApplicationRecord
       'default-avatar.png'
     end
   end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
+  end
+
 end
