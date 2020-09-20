d5e8062433dcba1ee8052056a8feede2c5f13bc5
saqla <whitecross8fortune8fatedress@gmail.com>
2020-09-20 11:24:38 +0900

    API完成 - !! パスの囲い`(バッククォーツ)に注意 '(シングルクォーツ)だとエラーになる

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -17,7 +17,7 @@ class LikesController < ApplicationController
   def destroy
     article = Article.find(params[:article_id])
     like = article.likes.find_by!(user_id: current_user.id)
-    
+
     like.destroy!
     redirect_to article_path(article)
   end
